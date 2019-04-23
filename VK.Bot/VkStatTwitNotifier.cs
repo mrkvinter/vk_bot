96923766db9e86dc7e77490581c391f6a26bb82d
mr.kvinter <mr.kvinter@gmail.com>
2019-04-23 11:42:37 +0500

    If profile is private then catch exception

--- VK.Bot/VkStatTwitNotifier.cs
+++ VK.Bot/VkStatTwitNotifier.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using VkNet.Abstractions;
+using VkNet.Exception;
+using VkNet.Model;
 using VkNet.Model.RequestParams;
 using VK.Bot.Extensions;
 using VK.Bot.Models;
@@ -32,17 +34,17 @@ namespace VK.Bot
             if (resultAuthorize.WasError)
                 return FoundResult<string>.Error($"Не удалось авторизоваться. {resultAuthorize.MessageError}");
 
-            var foundResult = vkApi.Users.TryGet(target);
-
-            if (foundResult.WasError)
+            var foundResultUser = vkApi.Users.TryGet(target);
+            if (foundResultUser.WasError)
                 return FoundResult<string>.Error($"Во время поиска пользователя {target} произошла ошибка.");
 
-            var userInfo = foundResult.Value;
+            var userInfo = foundResultUser.Value;
 
-            //todo: Обработать. У пользователя может быть закрыта стена.
-            var stat = twitStatCollector.Collect(userInfo.Id);
+            var foundResultStat = TryCollectCharsStat(userInfo);
+            if (foundResultStat.WasError)
+                return FoundResult<string>.Error(foundResultStat.MessageError);
 
-            var json = JsonConvert.SerializeObject(stat
+            var json = JsonConvert.SerializeObject(foundResultStat.Value
                 .Select(e => new KeyValuePair<char, double>(e.Key, Math.Round(e.Value, 2)))
                 .ToDictionary(e => e.Key, e => e.Value));
 
@@ -53,6 +55,22 @@ namespace VK.Bot
             });
 
             return FoundResult<string>.Success(json);
+        }
+
+        private FoundResult<Dictionary<char, double>> TryCollectCharsStat(User userInfo)
+        {
+            try
+            {
+                var stat = twitStatCollector.Collect(userInfo.Id);
+                return FoundResult<Dictionary<char, double>>.Success(stat);
+            }
+            catch (VkApiException e)
+            {
+                {
+                    return FoundResult<Dictionary<char, double>>.Error(e.Message);
+                }
+            }
+
         }
     }
 }
